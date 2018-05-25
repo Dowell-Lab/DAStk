1c6f654c5eb63590025545994dd3d2ad02d731a0
Ignacio Tripodi <ignaciot@gmail.com>
2018-05-24 20:42:07 -0600

    New PyPI version - Enforce the motif file extension to match either bed, BedGraph or txt to prevent from accidentally trying to parse other files in that same directory.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='DAStk',
-    version='0.1.4',
+    version='0.1.5',
     description='Differential ATAC-seq toolkit',
     long_description=long_description,
     license='BSD',
